b8483c5eaa7a2369e27de717326b6b86e2bc1c77
Kate Yu <kate@gumroad.com>
2015-06-30 13:41:06 -0700

    Update script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# !/bin/bash +ex
 echo "Building new docker image."
 docker build -t=sqlpad --rm=true .
 docker save -o sqlpad.tar sqlpad
@@ -8,9 +8,7 @@ echo "Sending docker image to remote."
 scp sqlpad.tar.gz $USER@$REMOTE_HOST_ADDRESS:/tmp
 
 echo "Checking for old image"
-ssh $USER@$REMOTE_HOST_ADDRESS "docker ps | grep sqlpad-instance"
-
-if [$? -eq 0]; then
+if ssh $USER@$REMOTE_HOST_ADDRESS "docker ps | grep sqlpad-instance &>/dev/null"; then
     echo "Deleting old image"
     ssh $USER@$REMOTE_HOST_ADDRESS "docker stop sqlpad-instance"
     ssh $USER@$REMOTE_HOST_ADDRESS "docker rm sqlpad-instance"
@@ -19,14 +17,17 @@ fi
 
 echo "Importing image"
 ssh $USER@$REMOTE_HOST_ADDRESS "cd /tmp && tar -xzvf /tmp/sqlpad.tar.gz"
-ssh $USER@$REMOTE_HOST_ADDRESS "docker load < /tmp/sqlpad.tar"
 
-if [$? -eq 0]; then
+
+if ssh $USER@$REMOTE_HOST_ADDRESS "docker load < /tmp/sqlpad.tar"; then
     echo "Running container"
-    # These env variables come from your own machine, not the remote
-    ssh $USER@$REMOTE_HOST_ADDRESS "docker run --name sqlpad-instance -p 4000:8080 -e GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID -e GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET -e PUBLIC_URL=$PUBLIC_URL -v /home/s_sqlpad/db:/sqlpad/db sqlpad"
-
-    if [$? -eq 0]; then 
+    if ssh $USER@$REMOTE_HOST_ADDRESS "docker run -d --name sqlpad-instance -p 4000:8080 \
+                                                  -e GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID \
+                                                  -e GOOGLE_CLIENT_SECRET=$GOOGLE_CLIENT_SECRET \
+                                                  -e PUBLIC_URL=$PUBLIC_URL \
+                                                  -e DISABLE_USERPASS_AUTH=true \
+                                                  --restart=on-failure:3 \
+                                                  -v /home/s_sqlpad/db:/sqlpad/db sqlpad"; then 
         echo "Removing temp files."
         rm sqlpad.tar
         rm sqlpad.tar.gz
